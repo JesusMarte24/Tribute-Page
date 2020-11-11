1074a6549bd72cd9bf80618f629879269789b1fa
Jesus Marte <jesusmarte2430@gmail.com>
2020-11-10 20:50:22 -0500

    styles for buttons to select plans has been added

--- assets/index.js
+++ assets/index.js
@@ -10,3 +10,19 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+let buttonIndividual = document.getElementById("plan--selection--1");
+let buttonTeam = document.getElementById("plan--selection--2");
+
+buttonIndividual.addEventListener("click", individualClass);
+buttonTeam.addEventListener("click", teamClass);
+
+function individualClass() {
+  buttonIndividual.classList.toggle("individual--actived");
+  buttonIndividual.classList.toggle("plan--selection");
+}
+
+function teamClass() {
+  buttonTeam.classList.toggle("plan--selection");
+  buttonTeam.classList.toggle("team--actived");
+}
