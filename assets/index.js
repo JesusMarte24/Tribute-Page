e5bbfcb740b344723cb07bc04eda8240205dc6cb
Jesus Marte <jesusmarte2430@gmail.com>
2020-11-13 21:35:58 -0500

    Javascript to changed between plan selection

--- assets/index.js
+++ assets/index.js
@@ -11,18 +11,42 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
 }
 
-let buttonIndividual = document.getElementById("plan--selection--1");
-let buttonTeam = document.getElementById("plan--selection--2");
+const BUTTON__INDIVIDUAL = document.getElementById("plan--selection--1");
+const BUTTON__TEAM = document.getElementById("plan--selection--2");
+const INDIVIDUALSECTION = document.querySelector(".plans__individual");
+const TEAMSECTION = document.querySelector(".plans__team");
 
-buttonIndividual.addEventListener("click", individualClass);
-buttonTeam.addEventListener("click", teamClass);
+BUTTON__INDIVIDUAL.addEventListener("click", individualClass);
+BUTTON__TEAM.addEventListener("click", teamActived);
 
 function individualClass() {
-  buttonIndividual.classList.toggle("individual--actived");
-  buttonIndividual.classList.toggle("plan--selection");
+  BUTTON__INDIVIDUAL.classList.toggle("individual--actived");
+  BUTTON__INDIVIDUAL.classList.toggle("plan--selection");
+
+  if (INDIVIDUALSECTION.classList.contains("individual__show--container")) {
+    INDIVIDUALSECTION.classList.remove("individual__show--container");
+  }
+
+  if (!TEAMSECTION.classList.contains("team__show--container")) {
+    TEAMSECTION.classList.add("team__show--container");
+  }
+
+  BUTTON__TEAM.classList.toggle("plan--selection");
+  BUTTON__TEAM.classList.toggle("team--actived");
 }
 
-function teamClass() {
-  buttonTeam.classList.toggle("plan--selection");
-  buttonTeam.classList.toggle("team--actived");
+function teamActived() {
+  BUTTON__TEAM.classList.toggle("plan--selection");
+  BUTTON__TEAM.classList.toggle("team--actived");
+
+  if (TEAMSECTION.classList.contains("team__show--container")) {
+    TEAMSECTION.classList.remove("team__show--container");
+  }
+
+  if (!INDIVIDUALSECTION.classList.contains("individual__show--container")) {
+    INDIVIDUALSECTION.classList.add("individual__show--container");
+  }
+
+  BUTTON__INDIVIDUAL.classList.toggle("individual--actived");
+  BUTTON__INDIVIDUAL.classList.toggle("plan--selection");
 }
